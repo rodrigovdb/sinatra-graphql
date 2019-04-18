8c1578ed1ded764984f1694eb3d3028047f44ab2
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2019-04-18 17:14:43 -0300

    Remove legacy seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,6 @@
 
 require 'faker'
 
-if Speaker.count.zero?
-  Speaker.create(name: 'Rodrigo', twitter_handle: 'rodrigovdb', bio: 'This is Rodrigo\'s bio', talk_title: 'How to get it running!')
-
-  Speaker.create(name: 'John', twitter_handle: 'johnruby', bio: 'This is John\'s bio', talk_title: 'How to bootstrap a sinatra application')
-
-  Speaker.create(name: 'Jacob', twitter_handle: 'jacob-ruby', bio: 'This is Jacob\'s bio', talk_title: 'Introduction to graphql')
-end
-
 if Artist.count.zero?
   10.times do
     artist = Artist.create(name: Faker::Music.unique.band)
