a4298f981c0adb40ae3dce701f3de4f92e76b5de
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2019-04-23 14:37:23 -0300

    Redefines types in a single file with all relationships

--- app/graphql/types.rb
+++ app/graphql/types.rb
@@ -1,3 +1,27 @@
 # frozen_string_literal: true
 
-Dir.glob(Pathname(__FILE__).sub_ext('/*.rb'), &method(:require))
+require 'graphql'
+
+module Types
+  class Artist < GraphQL::Schema::Object
+    description 'Artist Object Type'
+
+    field :id,    ID, null: false
+    field :name,  String, null: false
+  end
+
+  class Song < GraphQL::Schema::Object
+    description 'Artist Object Type'
+
+    field :id,        ID, null: false
+    field :name,      String, null: false
+    field :key,       String, null: true
+    field :duration,  Integer, null: true
+    field :chords,    String, null: true
+    field :artist,    Artist, null: false
+  end
+
+  class Artist < GraphQL::Schema::Object
+    field :songs, [Song], null: true
+  end
+end
