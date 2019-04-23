02eca89e87c69b756728cecea41164699c8bf63c
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2019-04-23 17:33:07 -0300

    Mutations to Artist and Songs

--- app/graphql/mutations.rb
+++ app/graphql/mutations.rb
@@ -3,21 +3,17 @@
 require 'graphql'
 
 require_relative 'types'
-
-module Mutations
-  class CreateArtist < GraphQL::Schema::Mutation
-    argument :name, String, required: true
-
-    type ::Types::ArtistType
-
-    def resolve(name:)
-      Artist.find_or_create_by(name: name)
-    end
-  end
-end
+require_relative 'mutations/artists_mutation'
+require_relative 'mutations/songs_mutation'
 
 module Types
   class MutationType < GraphQL::Schema::Object
     field :create_artist, mutation: ::Mutations::CreateArtist
+    field :update_artist, mutation: ::Mutations::UpdateArtist
+    field :delete_artist, mutation: ::Mutations::DeleteArtist
+
+    field :create_song,   mutation: ::Mutations::CreateSong
+    field :update_song,   mutation: ::Mutations::UpdateSong
+    field :delete_song,   mutation: ::Mutations::DeleteSong
   end
 end
